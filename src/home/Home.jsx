0d529a665a58ff694c780989cdd9562f00c23dce
Dag Frode Solberg <tncditto@gmail.com>
2016-11-16 23:16:04 +0100

    Updated readme with introduction

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -13,13 +13,10 @@ const Home = () => {
                 <img src={"http://animal-dream.com/data_images/monkey/monkey3.jpg"} alt={"Cute monkey"}/>
                 <figcaption>{"Planet of the Monkeys"}</figcaption>
             </figure>
-
-
             <figure className={"image-container small white"}>
                 <img src={"https://upload.wikimedia.org/wikipedia/commons/0/07/Giraffe08_-_melbourne_zoo.jpg"} alt={"Cute monkey"}/>
                 <figcaption>{"There's a giraffe in my loft!"}</figcaption>
             </figure>
-
             <figure className={"image-container medium black"}>
                 <img src={"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRToWlBIIT3JOGPvGMk7C7lseTY-GPI9tvK3tmeqHsvAVcI0r5lQw"} alt={"Cute monkey"}/>
                 <figcaption>{"My horse is amazing!"}</figcaption>
