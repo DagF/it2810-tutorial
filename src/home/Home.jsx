375a486c66a4611c46646338c792de876bcc7ea0
Dag Frode Solberg <tncditto@gmail.com>
2016-11-16 14:56:45 +0100

    Made component messy

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,33 +1,29 @@
 import React from 'react'
-import ImageBox from '../common/ImageBox/ImageBox'
 
 import './home.scss'
 
 const Home = () => {
     return (
         <div className="container">
+            <figure className={"image-container large red"}>
+                <img src={"https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg"} alt={"Burrito cat"}/>
+                <figcaption>{"What does a cat burrito taste like?"}</figcaption>
+            </figure>
+            <figure className={"image-container medium transparent"}>
+                <img src={"http://animal-dream.com/data_images/monkey/monkey3.jpg"} alt={"Cute monkey"}/>
+                <figcaption>{"Planet of the Monkeys"}</figcaption>
+            </figure>
 
-            <ImageBox url="https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg"
-                      alt="Burrito cat"
-                      text="What does a cat burrito taste like?"
-                      size="large"
-                      boxColor="red" />
 
-            <ImageBox url="http://animal-dream.com/data_images/monkey/monkey3.jpg"
-                      alt="Cute monkey"
-                      text="Planet of the Monkeys"/>
+            <figure className={"image-container small white"}>
+                <img src={"https://upload.wikimedia.org/wikipedia/commons/0/07/Giraffe08_-_melbourne_zoo.jpg"} alt={"Cute monkey"}/>
+                <figcaption>{"There's a giraffe in my loft!"}</figcaption>
+            </figure>
 
-            <ImageBox url="https://upload.wikimedia.org/wikipedia/commons/0/07/Giraffe08_-_melbourne_zoo.jpg"
-                      alt="Cute monkey"
-                      text="There's a giraffe in my loft!"
-                      size="small"
-                      boxColor="white"/>
-
-            <ImageBox url="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRToWlBIIT3JOGPvGMk7C7lseTY-GPI9tvK3tmeqHsvAVcI0r5lQw"
-                      alt="Cute monkey"
-                      text="My horse is amazing!"
-                      boxColor="black"
-                />
+            <figure className={"image-container medium black"}>
+                <img src={"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRToWlBIIT3JOGPvGMk7C7lseTY-GPI9tvK3tmeqHsvAVcI0r5lQw"} alt={"Cute monkey"}/>
+                <figcaption>{"My horse is amazing!"}</figcaption>
+            </figure>
         </div>
     )
 };
